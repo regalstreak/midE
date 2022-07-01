ed611f79244ed49449e05dbcc6fc58ba06a4a4db
Neil Agarwal <neil@neilagarwal.me>
2022-07-01 20:39:27 +0530

    feat: android usb midi driver fixes

--- app/src/main/java/me/neilagarwal/midE/MainActivity.java
+++ app/src/main/java/me/neilagarwal/midE/MainActivity.java
@@ -1,14 +1,185 @@
 package me.neilagarwal.midE;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import jp.kshoji.driver.midi.activity.AbstractSingleMidiActivity;
+import jp.kshoji.driver.midi.device.MidiInputDevice;
+import jp.kshoji.driver.midi.device.MidiOutputDevice;
+
+public class MainActivity extends AbstractSingleMidiActivity {
+
+    // this field belongs to the UI thread
+    final Handler uiThreadEventHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            if ("note on".equals(msg.obj)) {
+                textView.setText("note on event received." + msg.obj.toString());
+            }
+            Log.v("testing", msg.obj.toString() + "testing");
+
+            // message handled successfully
+            return true;
+        }
+    });
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.testing);
+    }
+
+    // this method will be called from the another thread, so it can't change View's state.
+    @Override
+    public void onMidiNoteOn(final MidiInputDevice sender, int cable, int channel, int note, int velocity) {
+        // Send a message to the UI thread
+        String message = "note on";
+        uiThreadEventHandler.sendMessage(Message.obtain(uiThreadEventHandler, 0, message));
+    }
+
+    @Override
+    public void onMidiMiscellaneousFunctionCodes(@NonNull MidiInputDevice sender, int cable, int byte1, int byte2, int byte3) {
+
+    }
+
+    @Override
+    public void onMidiCableEvents(@NonNull MidiInputDevice sender, int cable, int byte1, int byte2, int byte3) {
+
+    }
+
+    @Override
+    public void onMidiSystemCommonMessage(@NonNull MidiInputDevice sender, int cable, byte[] bytes) {
+
+    }
+
+    @Override
+    public void onMidiSystemExclusive(@NonNull MidiInputDevice sender, int cable, byte[] systemExclusive) {
+
+    }
+
+    @Override
+    public void onMidiNoteOff(@NonNull MidiInputDevice sender, int cable, int channel, int note, int velocity) {
+
+    }
+
+
+    @Override
+    public void onMidiPolyphonicAftertouch(@NonNull MidiInputDevice sender, int cable, int channel, int note, int pressure) {
+
+    }
+
+    @Override
+    public void onMidiControlChange(@NonNull MidiInputDevice sender, int cable, int channel, int function, int value) {
+
+    }
+
+    @Override
+    public void onMidiProgramChange(@NonNull MidiInputDevice sender, int cable, int channel, int program) {
+
+    }
+
+    @Override
+    public void onMidiChannelAftertouch(@NonNull MidiInputDevice sender, int cable, int channel, int pressure) {
+
+    }
+
+    @Override
+    public void onMidiPitchWheel(@NonNull MidiInputDevice sender, int cable, int channel, int amount) {
+
+    }
+
+    @Override
+    public void onMidiSingleByte(@NonNull MidiInputDevice sender, int cable, int byte1) {
+
+    }
+
+    @Override
+    public void onMidiTimeCodeQuarterFrame(@NonNull MidiInputDevice sender, int cable, int timing) {
+
+    }
+
+    @Override
+    public void onMidiSongSelect(@NonNull MidiInputDevice sender, int cable, int song) {
+
+    }
+
+    @Override
+    public void onMidiSongPositionPointer(@NonNull MidiInputDevice sender, int cable, int position) {
+
+    }
+
+    @Override
+    public void onMidiTuneRequest(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiTimingClock(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiStart(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiContinue(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiStop(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiActiveSensing(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onMidiReset(@NonNull MidiInputDevice sender, int cable) {
+
+    }
+
+    @Override
+    public void onDeviceAttached(@NonNull UsbDevice usbDevice) {
+
+    }
+
+    @Override
+    public void onMidiInputDeviceAttached(@NonNull MidiInputDevice midiInputDevice) {
+
+    }
+
+    @Override
+    public void onMidiOutputDeviceAttached(@NonNull MidiOutputDevice midiOutputDevice) {
+
+    }
+
+    @Override
+    public void onDeviceDetached(@NonNull UsbDevice usbDevice) {
+
+    }
+
+    @Override
+    public void onMidiInputDeviceDetached(@NonNull MidiInputDevice midiInputDevice) {
+
+    }
+
+    @Override
+    public void onMidiOutputDeviceDetached(@NonNull MidiOutputDevice midiOutputDevice) {
+
     }
 }
