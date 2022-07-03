ada6e12f669e421ad5ef388a25a3ce96410c02a9
Neil Agarwal <neil@neilagarwal.me>
2022-07-04 03:59:35 +0530

    feat: viewmodel, jetpack compose

---
+++ app/src/main/java/me/neilagarwal/midE/MainViewModel.kt
@@ -0,0 +1,13 @@
+package me.neilagarwal.midE
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    val noDeviceDeviceName: String = "[none]"
+
+    var usbMidiDeviceName by mutableStateOf(noDeviceDeviceName)
+    var bleMidiDevicename by mutableStateOf(noDeviceDeviceName)
+}
